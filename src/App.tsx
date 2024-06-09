d4c3bf64716d730cd27601b8d586ef5c06c41e62
Nika M. <merabishvilinikusha@gmail.com>
2024-06-09 23:19:51 +0400

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { MoonIcon } from "@heroicons/react/16/solid";
+
+function App() {
+  return (
+    <>
+      <p className="">react</p>
+
+      <MoonIcon className="size-6" />
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['"Nunito Sans"', "sans-serif"],
+      },
+      fontWeight: {
+        light: 300,
+        semiBold: 600,
+        extraBold: 800,
+      },
+      colors: {
+        darkBlue: "hsl(209, 23%, 22%)",
+        veryDarkBlueBg: "hsl(207, 26%, 17%)",
+        veryDarkBlueText: "hsl(200, 15%, 8%)",
+        darkGray: "hsl(0, 0%, 52%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
